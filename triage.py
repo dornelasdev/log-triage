0a0e6aeb0b6a85c01d93963ff58318d34a97e05e
dornelasdev <dornelas@MacBook-Air-de-Joao.local>
2026-03-02 18:05:55 +0000

    Refactor regex and prepare for multi-log support

--- triage.py
+++ triage.py
@@ -2,14 +2,31 @@ import csv
 import json
 import os
 import re
+import ipaddress
+
+log_file = "sample-logs/auth.log"
+fieldnames = ["timestamp", "hostname", "service", "pid", "event_type",
+                "user_validity", "username", "source_ip", "source_port", "protocol", "message"]
+regex_timestamp = re.compile(r"^[A-Z][a-z]{2}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2}")
+header_pattern = re.compile(
+        r"^(?P<timestamp>[A-Z][a-z]{2}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+"
+        r"(?P<hostname>\S+)\s+"
+        r"(?P<service>\w+)\[(?P<pid>\d+)\]:\s+"
+        r"(?P<message>.+)$"
+)
+ssh_message_pattern = re.compile(
+        r"^(?P<event_type>Accepted|Failed) password for " # captures if "Accepted" or "Failed"
+        r"(?:(?P<user_validity>invalid) user )?" # "invalid user", group user_validity
+        r"(?P<username>\S+) " # username
+        r"from (?P<source_ip>\d+\.\d+\.\d+\.\d+) " # IPv4
+        r"port (?P<source_port>\d+) " # port
+        r"(?P<protocol>\S+)$" # protocol, usually ssh2
+)
 
 # MAIN FUNCTION
 def main():
     events = []
     lines_skipped = 0
-    log_file = "sample-logs/auth.log"
-
-    regex_timestamp = re.compile(r"^[A-Z][a-z]{2}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2}")
 
     with open(log_file, "r") as file:
         for line in file:
@@ -39,9 +56,6 @@ def main():
     with open("outputs/parsed_events.json", "w") as json_file:
         json.dump(events, json_file, indent=4)
 
-    fieldnames = ["timestamp", "hostname", "service", "pid", "event_type",
-                    "user_validity", "username", "source_ip", "source_port", "protocol", "message"]
-
     with open("outputs/parsed_events.csv", "w", newline="") as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
@@ -50,13 +64,6 @@ def main():
     print(f"{lines_skipped} line(s) were not properly parsed.")
 
 def parse_log_line(line):
-
-    header_pattern = re.compile(
-        r"^(?P<timestamp>[A-Z][a-z]{2}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+"
-        r"(?P<hostname>\S+)\s+"
-        r"(?P<service>\w+)\[(?P<pid>\d+)\]:\s+"
-        r"(?P<message>.+)$"
-    )
 
     match = header_pattern.match(line)
 
@@ -73,16 +80,7 @@ def parse_log_line(line):
 
 def parse_message_ssh(str_message):
 
-    pattern = re.compile(
-        r"^(?P<event_type>Accepted|Failed) password for " # captures if "Accepted" or "Failed"
-        r"(?:(?P<user_validity>invalid) user )?" # "invalid user", group user_validity
-        r"(?P<username>\S+) " # username
-        r"from (?P<source_ip>\d+\.\d+\.\d+\.\d+) " # IPv4
-        r"port (?P<source_port>\d+) " # port
-        r"(?P<protocol>\S+)$" # protocol, usually ssh2
-    )
-
-    match = pattern.match(str_message)
+    match = ssh_message_pattern.match(str_message)
     if not match:
         return None
 
